7b8305b5b6a7fbf86b647bfd046d605e1ca59d09
ahmadfahrezi <ahmadfahrezi8127@gmail.com>
2025-01-25 23:16:55 +0800

    remove duplicated next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.js$/,
+            include: /node_modules\/undici/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"],
+                },
+            },
+        });
+        return config;
+    },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
